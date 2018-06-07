e39861a9327ddf354599799d3f222dceb1824d0b
karencplara <34969669+karencplara@users.noreply.github.com>
2018-06-07 20:12:15 -0300

    Initial commit

---
+++ ConsumindoApi/Controllers/NotesController.cs
@@ -0,0 +1,145 @@
+﻿using ConsumindoApi.Models.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ConsumindoApi.Controllers
+{
+    public class NotesController : Controller
+    {
+        HttpClient Client = new HttpClient();
+        public NotesController()
+        {
+            Client.BaseAddress = new Uri("http://devmedianotesapi.azurewebsites.net");
+            Client.DefaultRequestHeaders.Accept.Clear(); //limpar headers
+            Client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json")); //adc formato json 
+        }
+        // GET: Notes
+        public ActionResult Index()
+        {
+            List<Note> notes = new List<Note>();
+            HttpResponseMessage response = Client.GetAsync("api/notes").Result;
+            if (response.IsSuccessStatusCode)
+            {
+                notes = response.Content.ReadAsAsync<List<Note>>().Result;
+            }
+            return View(notes);
+        }
+
+        // GET: Notes/Details/5
+        public ActionResult Details(int id)
+        {
+            HttpResponseMessage response = Client.GetAsync($"/api/notes/{id}").Result;
+            Note note = response.Content.ReadAsAsync<Note>().Result;
+            if (note != null)
+                return View(note);
+            else
+                return HttpNotFound();
+        }
+
+        // GET: Notes/Create
+        public ActionResult Create()
+        {
+          
+            return View();
+        }
+
+        // POST: Notes/Create
+        [HttpPost]
+        public ActionResult Create(Note note)
+        {
+            try
+            {
+                HttpResponseMessage response = Client.PostAsJsonAsync<Note>("/api/notes", note).Result;
+                if(response.StatusCode == System.Net.HttpStatusCode.Created)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    ViewBag.Error = "Erro ao criar a nota.";
+                    return View();
+                }
+                
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Notes/Edit/5
+        public ActionResult Edit(int id)
+        {
+            HttpResponseMessage response = Client.GetAsync($"/api/notes/{id}").Result;
+            Note note = response.Content.ReadAsAsync<Note>().Result;
+            if (note != null)
+                return View(note);
+            else
+                return HttpNotFound();
+        }
+
+        // POST: Notes/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, Note note)
+        {
+            try
+            {
+                HttpResponseMessage response = Client.PutAsJsonAsync<Note>($"/api/notes/{id}", note).Result;
+                if (response.StatusCode == System.Net.HttpStatusCode.NoContent)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    ViewBag.Error = "Erro ao editar a nota.";
+                    return View();
+                }
+
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Notes/Delete/5
+        public ActionResult Delete(int id)
+        {
+            HttpResponseMessage response = Client.GetAsync($"/api/notes/{id}").Result;
+            Note note = response.Content.ReadAsAsync<Note>().Result;
+            if (note != null)
+                return View(note);
+            else
+                return HttpNotFound();
+        }
+
+        // POST: Notes/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                HttpResponseMessage response = Client.DeleteAsync($"/api/notes/{id}").Result;
+                if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    ViewBag.Error = "Erro ao excluir a nota.";
+                    return View();
+                }
+
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ ConsumindoApi/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ConsumindoApi.MvcApplication" Language="C#" %>
